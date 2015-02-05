0f04fb60fa8bd464722d289221bf21a9b1dacb8d
flametuner <flame.tuner@gmail.com>
2015-02-04 23:38:36 -0200

    Se o player esta mutado, nao pode falar, rever sistema de tempban/tempmute

--- yUtils/src/me/flame/utils/banmanager/listeners/LoginListener.java
+++ yUtils/src/me/flame/utils/banmanager/listeners/LoginListener.java
@@ -2,11 +2,13 @@ package me.flame.utils.banmanager.listeners;
 
 import me.flame.utils.banmanager.BanManagement;
 import me.flame.utils.banmanager.constructors.Ban;
+import me.flame.utils.banmanager.constructors.Mute;
 import net.md_5.bungee.api.ChatColor;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerLoginEvent.Result;
 
@@ -18,10 +20,29 @@ public class LoginListener implements Listener {
 	}
 
 	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		Player p = event.getPlayer();
+		if (!manager.isMuted(p))
+			return;
+		Mute mute = manager.getMute(p);
+		if (mute.hasExpired()) {
+			return;
+		}
+		// TODO Rever sistema de tempban
+		if (mute.getDuration().toString() == "0") {
+			p.sendMessage(ChatColor.YELLOW + "Voce foi mutado por " + mute.getMutedBy() + "! Motivo: " + ChatColor.AQUA + mute.getReason());
+		} else {
+			p.sendMessage(ChatColor.YELLOW + "Voce foi mutado por " + mute.getDuration().getTime() + " segundos pelo player " + mute.getMutedBy() + "! Motivo: " + ChatColor.AQUA + mute.getReason());
+		}
+		event.setCancelled(true);
+	}
+
+	@EventHandler
 	public void onLogin(PlayerLoginEvent event) {
 		Player p = event.getPlayer();
 		if (!manager.isBanned(p))
 			return;
+		// TODO Rever sistema de tempban
 		Ban ban = manager.getBan(p);
 		StringBuilder builder = new StringBuilder();
 		builder.append(ChatColor.YELLOW + "Voce foi banido do servidor!");
