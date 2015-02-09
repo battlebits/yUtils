fac9792b7133f4bc6c97d312e5d66adafd77ef5e
flametuner <flame.tuner@gmail.com>
2015-02-09 16:45:50 -0200

    Inicio Payments

--- yUtils/src/me/flame/utils/payment/BuyManager.java
+++ yUtils/src/me/flame/utils/payment/BuyManager.java
@@ -1,9 +1,13 @@
 package me.flame.utils.payment;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 import me.flame.utils.Main;
 import me.flame.utils.Management;
 
 public class BuyManager extends Management {
+	private HashMap<UUID, Long> expires;
 
 	public BuyManager(Main main) {
 		super(main);
@@ -11,6 +15,7 @@ public class BuyManager extends Management {
 
 	@Override
 	public void onEnable() {
+		expires = new HashMap<>();
 
 	}
 
