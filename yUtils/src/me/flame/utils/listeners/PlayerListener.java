7eb95038cc1fb10d85a9ca9690f14ee3a0cb4672
flametuner <flame.tuner@gmail.com>
2016-02-08 10:53:06 -0200

    Atualizado comando /me

--- yUtils/src/me/flame/utils/listeners/PlayerListener.java
+++ yUtils/src/me/flame/utils/listeners/PlayerListener.java
@@ -37,7 +37,7 @@ public class PlayerListener implements Listener {
 
 	@EventHandler
 	public void onPreProcessCommand(PlayerCommandPreprocessEvent event) {
-		if (event.getMessage().toLowerCase().contains("/me")) {
+		if (event.getMessage().toLowerCase().startsWith("/me ")) {
 			event.getPlayer().sendMessage(ChatColor.RED + "Voce nao pode utilizar o comando 'me'");
 			event.setCancelled(true);
 		}
